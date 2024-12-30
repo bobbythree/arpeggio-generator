a02e8088d0b50a1581ed6de23d57d90d9ed136dc
padesso <patrickadesso@gmail.com>
2024-12-30 09:32:52 -0500

    Keyboard playing chord arps

--- main.js
+++ main.js
@@ -1,44 +1,14 @@
 //setup music functionality
 const synth = new Tone.PolySynth(Tone.Synth).toDestination();
 const c4_frequency = 261.626;
-
-const chords = {
-  I: {
-    chord: 'I - C3',
-    rootNoteSemitonesFromC4 : -7  
-  },
-  ii: {
-    chord: 'ii - D3',
-    rootNoteSemitonesFromC4 : -5
-  },
-  iii: {
-    chord: 'iii - E3',
-    rootNoteSemitonesFromC4 : -3
-  },
-  IV: {
-    chord: 'IV - F3', 
-    rootNoteSemitonesFromC4 : -2
-  },
-  V: {
-    chord: 'V - G3',
-    rootNoteSemitonesFromC4 : 0
-  },
-  vi: {
-    chord: 'vi - A3',
-    rootNoteSemitonesFromC4 : +2
-  },
-  vii: {
-    chord: 'vii - B3',
-    rootNoteSemitonesFromC4 : +4
-  },
-}
+let currentOctave = 4;
 
 //Intervals in semitones to play this type of chord
 const chordIntervals = {
-  major: [0, 4, 5, 7],
-  minor: [0, 3, 5, 7],
-  diminished: [0, 3, 5, 6],
-  augmented: [0, 4, 5, 8],
+  major: [0, 4, 7],
+  minor: [0, 3, 7],
+  diminished: [0, 3, 6],
+  augmented: [0, 4, 8],
   major7: [0, 4, 7, 11],
   minor7: [0, 3, 7, 10],
   dominant7: [0, 4, 7, 10],
@@ -47,8 +17,8 @@ const chordIntervals = {
   minorMajor7: [0, 3, 7, 11],
   augmentedMajor7: [0, 4, 8, 11],
   augmented7: [0, 4, 8, 10],
-  sus2: [0, 2, 5, 7],
-  sus4: [0, 5, 7, 12],
+  sus2: [0, 2, 7],
+  sus4: [0, 5, 7],
   add9: [0, 4, 7, 14],
   add11: [0, 4, 7, 17],
   add13: [0, 4, 7, 21]
@@ -70,14 +40,8 @@ dropdownDiv.appendChild(dropdown);
 
 //add event listener to dropdown
 dropdown.addEventListener('change', (event) => {
-  const selectedChord = event.target.value;
-  if (selectedChord === 'I' || selectedChord === 'IV') {
-    playArp(chords[selectedChord].rootNoteSemitonesFromC4);
-  }
+  selectedChord = event.target.value;
 });
-
-//create buttons with functionality
-const buttonDiv = document.getElementById('button-div');
 
 //helper function to calculate frequency of notes offset from C4
 function calculateFrequency(semitonesFromC4) {
@@ -85,24 +49,50 @@ function calculateFrequency(semitonesFromC4) {
   return noteFrequency;
 }
 
-//draw the buttons
-for (const chord in chords) {
-  const button = document.createElement('button');
-  button.innerHTML = chords[chord].chord;
-  buttonDiv.appendChild(button);
-  button.onclick = () => {
-    if(chord === 'I' || chord === 'IV') {
-      playArp(chords[chord].rootNoteSemitonesFromC4);
-    }
-  }
-}
-
-function playArp(semitonesFromC4) {
+function playArp(semitonesFromC4, chord) {
   console.info('Playing Arp from root note: ' + calculateFrequency(semitonesFromC4));
   const now = Tone.now();
   var timeOffset = .25;
-  synth.triggerAttackRelease(calculateFrequency(semitonesFromC4), "2n", now);
-  synth.triggerAttackRelease(calculateFrequency(semitonesFromC4 + 4), "8n", now + timeOffset);
-  synth.triggerAttackRelease(calculateFrequency(semitonesFromC4 + 7), "8n", now + timeOffset  * 2);
-  synth.triggerAttackRelease(calculateFrequency(semitonesFromC4 + 11), "8n", now + timeOffset  * 3);
+  synth.triggerAttackRelease(calculateFrequency(semitonesFromC4), "4n", now);
+  chordIntervals[chord].slice(1).forEach((interval, index) => {
+    synth.triggerAttackRelease(calculateFrequency(semitonesFromC4 + interval), "8n", now + timeOffset * (index + 1));
+  });
 }
+
+// Keyboard and octave controls
+const notes = {
+  'C': 0,
+  'C#': 1,
+  'D': 2,
+  'D#': 3,
+  'E': 4,
+  'F': 5,
+  'F#': 6,
+  'G': 7,
+  'G#': 8,
+  'A': 9,
+  'A#': 10,
+  'B': 11
+};
+
+let selectedChord = 'major'; // Default chord
+
+document.querySelectorAll('.note').forEach(button => {
+  button.addEventListener('click', () => {
+    const note = button.getAttribute('data-note');
+    const semitonesFromC4 = notes[note] + (currentOctave - 4) * 12;
+    playArp(semitonesFromC4, selectedChord);
+  });
+});
+
+document.getElementById('decrease-octave').addEventListener('click', () => {
+  if (currentOctave > 1) {
+    currentOctave--;
+  }
+});
+
+document.getElementById('increase-octave').addEventListener('click', () => {
+  if (currentOctave < 7) {
+    currentOctave++;
+  }
+});
