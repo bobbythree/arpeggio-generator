f8738d1301506ef893052f3b9cd8ddc596a02f9c
padesso <patrickadesso@gmail.com>
2025-01-01 21:18:46 -0500

    Added placeholder chords for moods and some for happy and serene to play with when integrating audio and editor.

--- Editor/data/moods-chords.js
+++ Editor/data/moods-chords.js
@@ -1,38 +1,125 @@
+// To predict next note in a chord, we add 12 to each note in the array
+// For example, if the chord is Cmajor, the notes are [0, 4, 7, 12] and the next notes are [12, 16, 19, 24]
 const moods = {
 	"happy": [{
 			"chordName": "Cmajor9",
 			"chordIntervals": [0, 4, 7, 11, 14],
-			"rootNote": "C"
+			"rootNote": "C5"
 		},
 		{
 			"chordName": "Cmajor",
 			"chordIntervals": [0, 4, 7, 12],
-			"rootNote": "C"
+			"rootNote": "C4"
 		},
 		{
 			"chordName": "Gmajor",
 			"chordIntervals": [0, 4, 7, 12],
-			"rootNote": "G"
-		}
+			"rootNote": "G3"
+		},
+		{
+			"chordName": "C5",
+			"chordIntervals": [0, 7],
+			"rootNote": "C2"
+		},
 	],
-	"sad": {
-		"chordName": "minor",
-		"chordIntervals": [0, 3, 7]
-	},
-	"spooky": {
-		"chordName": "wholetone",
-		"chordIntervals": [0, 2, 4, 6, 8, 10, 12]
-	},
-	"serene": {
-		"chordName": "sus4",
-		"chordIntervals": [0, 5, 7, 12]
-	},
-	"nostalgic": {
-		"chordName": "minor11",
-		"chordIntervals": [0, 3, 7, 10, 14, 17]
-	},
-	"angry": {
-		"chordName": "diminished7",
-		"chordIntervals": [0, 3, 6, 9]
-	}
+	"sad": [{
+		"chordName": "Cmajor9",
+		"chordIntervals": [0, 4, 7, 11, 14],
+		"rootNote": "C5"
+		},
+		{
+			"chordName": "Cmajor",
+			"chordIntervals": [0, 4, 7, 12],
+			"rootNote": "C4"
+		},
+		{
+			"chordName": "Gmajor",
+			"chordIntervals": [0, 4, 7, 12],
+			"rootNote": "G3"
+		},
+		{
+			"chordName": "C5",
+			"chordIntervals": [0, 7],
+			"rootNote": "C2"
+		},
+	],
+	"spooky": [{
+		"chordName": "Cmajor9",
+		"chordIntervals": [0, 4, 7, 11, 14],
+		"rootNote": "C5"
+		},
+		{
+			"chordName": "Cmajor",
+			"chordIntervals": [0, 4, 7, 12],
+			"rootNote": "C4"
+		},
+		{
+			"chordName": "Gmajor",
+			"chordIntervals": [0, 4, 7, 12],
+			"rootNote": "G3"
+		},
+		{
+			"chordName": "C5",
+			"chordIntervals": [0, 7],
+			"rootNote": "C2"
+		},
+	],
+	"serene": [{
+		"chordName": "Csus4",
+		"chordIntervals": [0, 5, 7, 12],
+		"rootNote": "C5"
+		},
+		{
+			"chordName": "F5",
+			"chordIntervals": [0, 7],
+			"rootNote": "F4"
+		},
+		{
+			"chordName": "COctave",
+			"chordIntervals": [0, 12],
+			"rootNote": "C2"
+		},
+	],
+	"nostalgic": [{
+		"chordName": "Cmajor9",
+		"chordIntervals": [0, 4, 7, 11, 14],
+		"rootNote": "C5"
+		},
+		{
+			"chordName": "Cmajor",
+			"chordIntervals": [0, 4, 7, 12],
+			"rootNote": "C4"
+		},
+		{
+			"chordName": "Gmajor",
+			"chordIntervals": [0, 4, 7, 12],
+			"rootNote": "G3"
+		},
+		{
+			"chordName": "C5",
+			"chordIntervals": [0, 7],
+			"rootNote": "C2"
+		},
+	],
+	"angry": [{
+		"chordName": "Cmajor9",
+		"chordIntervals": [0, 4, 7, 11, 14],
+		"rootNote": "C5"
+		},
+		{
+			"chordName": "Cmajor",
+			"chordIntervals": [0, 4, 7, 12],
+			"rootNote": "C4"
+		},
+		{
+			"chordName": "Gmajor",
+			"chordIntervals": [0, 4, 7, 12],
+			"rootNote": "G3"
+		},
+		{
+			"chordName": "C5",
+			"chordIntervals": [0, 7],
+			"rootNote": "C2"
+		},
+	],
 }
