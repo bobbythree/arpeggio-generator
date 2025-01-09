e9144c44747500aa9c2cb4ea93d5300950994c84
padesso <patrickadesso@gmail.com>
2025-01-08 23:31:31 -0500

    Improved arp callback

--- Editor/audio/argeggiator.js
+++ Editor/audio/argeggiator.js
@@ -1,46 +1,47 @@
 //setup music functionality
 Tone.Transport.bpm.value = 120;
-Tone.Transport.start(1); //wait 1 second before starting
+Tone.Transport.start(); //wait 1 second before starting
+
+
 
 const arps = [];
 const synth = new Tone.PolySynth(Tone.Synth).toDestination();
-const timeOffset = .25; //TODO: do better losers
 
 export function addArp(arp) {
   arp.startBeat = getCurrentBeat();
-  arps.push(arp)
+  arps.push(arp);
 }
 
 export function start(){
   console.log("Starting Tone");
   Tone.Transport.start();
-  loop.start(0);
+  loop.start();
 }
 
 export function stop(){
   Tone.Transport.stop();
 } 
 
-//This is the loop fires on a 1m interval
-var loop = new Tone.Loop(loopCallback, "8n");
 
-function loopCallback(time){
+//This is the loop fires on a 1m interval
+var loop = new Tone.Loop( (time) => {
+  document.getElementById("start-button").innerHTML = getCurrentBeat();
 
     console.log("Playing Arps at beat: " + getCurrentBeat());
-    arps.forEach((arp) => {    
-
+    arps.forEach((arp) => {  
       if(arp.startBeat === getCurrentBeat()){
         arp.intervals.forEach((interval, index) => {
           if(index === 0){
             synth.triggerAttackRelease(Tone.Frequency(arp.rootNote), "2n", time);
           }
           else {
-            synth.triggerAttackRelease(Tone.Frequency(arp.rootNote).transpose(interval), "8n", time + timeOffset * index); 
+            synth.triggerAttackRelease(Tone.Frequency(arp.rootNote).transpose(interval), "8n", time + Tone.Time("8n") * index); 
           }
         });
       }
   });
-}
+}, "4n");
+
 
 function getCurrentBeat() {
   const position = Tone.Transport.position;
