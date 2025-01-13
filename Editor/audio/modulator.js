f77541ccd497cb8f3d9bc26f8ca2dfed86ece0ad
padesso <patrickadesso@gmail.com>
2025-01-13 18:46:44 -0500

    Stash before converting to an effect

--- Editor/audio/modulator.js
+++ Editor/audio/modulator.js
@@ -39,17 +39,19 @@ export function init(app) {
     app.stage.addChild(displacementSprite);
 
     // Create a Displacement Filter
-    const displacementFilter = new PIXI.filters.DisplacementFilter(displacementSprite);
-    waterSprite.filters = [displacementFilter];
+    //const displacementFilter = new PIXI.filters.DisplacementFilter(displacementSprite);
+    //waterSprite.filters = [displacementFilter];
 
     //TODO: control this from tone.js draw function to sync to LFO
     // Animate the displacement map
     let scaleDirectionX = 1;
     let scaleDirectionY = 1;
 
+
     app.ticker.add((delta) => {
-        displacementSprite.scale.x += scaleDirectionX * lfo.frequency.value * delta;
-        displacementSprite.scale.y += scaleDirectionY * lfo.frequency.value * delta;
+        
+        displacementSprite.scale.x += scaleDirectionX * lfo.frequency.value;
+        displacementSprite.scale.y += scaleDirectionY * lfo.frequency.value;
 
         if (displacementSprite.scale.x >= 10 || displacementSprite.scale.x <= 1) {
             scaleDirectionX *= -1;
@@ -57,12 +59,14 @@ export function init(app) {
         if (displacementSprite.scale.y >= 10 || displacementSprite.scale.y <= 1) {
             scaleDirectionY *= -1;
         }
+
+        displacementSprite.x = app.screen.width / 2;
+        displacementSprite.y = app.screen.height / 2;
+
+        console.log(displacementSprite.scale.y);
     });
 
     // Tone.js LFO for displacement control
-    const meter = new Tone.DCMeter();
-    
-    lfo.connect(meter);
     lfo.sync().start();
 
     //TODO: route this effect to a parameter of an arp synth that, controlling the wet value from distance to the center of the LFO
