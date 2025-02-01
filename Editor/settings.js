6e6f1a21bb3c3d9a3abae4ac371a97b7415fc16c
bobbythree <mynameisbobbylewis@hotmail.com>
2025-02-01 10:20:01 -0600

    toggle control panel display

--- Editor/settings.js
+++ Editor/settings.js
@@ -1,3 +1,16 @@
+//toggle debug panel display
+const debugPanel = document.getElementById('control-panel');
+debugPanel.style.display = 'block'
+
+export function toggleControlPanel () {
+    if (debugPanel.style.display === 'block') {
+        debugPanel.style.display = 'none';
+    } else {
+        debugPanel.style.display = 'block';
+    }
+}
+
+//toggle debug mode
 export const settings = {
     debugMode: false
 };
